4c4232ae2bd4db3c41e1889b71b81088bbe9af8e
J3dI2 <71358519+J3dI2@users.noreply.github.com>
2021-10-24 08:13:18 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import HomeScreen from './screens/HomeScreen';
+import HomeScreen from './screen/HomeScreen';
 
 export default class App extends Component{
   render(){
